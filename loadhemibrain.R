9e99c3decb9d7b296acf300b4a7e67a680762452
AlikiZ <38488804+AlikiZ@users.noreply.github.com>
2020-11-24 11:41:52 +0100

    Delete loadhemibrain.R

--- loadhemibrain.R
+++
@@ -1,3 +0,0 @@
-conn = neuprint_login(server= "https://neuprint.janelia.org/",
-                      token= "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InphdmFyb3BvdWxvdS5hbGlraUBnbWFpbC5jb20iLCJsZXZlbCI6Im5vYXV0aCIsImltYWdlLXVybCI6Imh0dHBzOi8vbGg2Lmdvb2dsZXVzZXJjb250ZW50LmNvbS8tQWJDLWx0S19mRzgvQUFBQUFBQUFBQUkvQUFBQUFBQUFBQUEvQU1adXVjay1hR1ViNjZ5UnVmVHpiaXhodWxrcVJ3blMxdy9zOTYtYy9waG90by5qcGc_c3o9NTA_c3o9NTAiLCJleHAiOjE3ODM5MDQ2NjJ9.YGVe1LLz_a_zWroPT87oLygD_Rcxom0gRIOMGI8Rz2w",
-                      dataset = "hemibrain:v1.1" )
