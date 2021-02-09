db583815a26712ca985ff9e78d6de2b7fb3497ac
AlikiZ <aliki_z@hotmail.com>
2021-02-09 13:49:45 +0100

    to fix

--- PCA_extended.R
+++ PCA_extended.R
@@ -9,6 +9,7 @@ library(dplyr)
 
 
 ### small functions ###
+
 PCAanalysis_metu <- function(a, namingplot){
   # use the nonzero columns 
   pdf(namingplot)
@@ -94,6 +95,7 @@ heatmap(MeTuconnect, scale = "col", Rowv = NA, Colv = NA)
 ## sub1_a: 1;  sub1_b: 2; sub2: 3; sub3: 4;  sub4: 5; BUT we put sub1_a and sub1_b together to sub1: 2
 subgr <- as.numeric(factor(group$subgroup))
 names(subgr) = group$bodyid
+# TODO: fix the cbind
 matMeTu = cbind(MeTuconnect, subgr)
 matMeTu = as.data.frame(matMeTu)
 ## convert the type of subgr so as to use the PCA library later on, merge sub1_a and sub1_b to sub1: 2
@@ -101,27 +103,27 @@ matMeTu$subgr <- as.character(matMeTu$subgr)
 matMeTu$subgr[matMeTu$subgr == 1] <- 2
 
 ## PCA analysis: scaled and not scaled --> both hold low percentage of explained variance in the first 5 PCs (25% and 40% respectively)
-# somepca <- PCA(MeTuconnect, scale.unit = TRUE, graph = FALSE)
-# fviz_pca_ind(somepca, geom.ind = "point", col.ind= matMeTu$subgr , palette = c("#00AFBB", "#E7B800", "orange", "green", "purple"), axes.linetype = "blank", legend.title="Group")
-# fviz_eig(somepca, addlabels = TRUE)
-# 
-# 
-# somepca <- PCA(MeTuconnect, scale.unit = FALSE, graph = FALSE)
-# fviz_pca_ind(somepca, geom.ind = "point", col.ind= matMeTu$subgr , palette = c("#00AFBB", "orange", "green", "purple"), axes.linetype = "blank", legend.title="Group")
-# fviz_pca_ind(somepca, geom.ind = "point", col.ind= matMeTu$subgr , palette = c("#00AFBB", "orange", "green", "purple"), axes.linetype = "blank", legend.title="Group",axes = c(1,3))
-# fviz_pca_ind(somepca, geom.ind = "point", col.ind= matMeTu$subgr , palette = c("#00AFBB", "orange", "green", "purple"), axes.linetype = "blank", legend.title="Group", axes = c(2,3))
-# fviz_pca_ind(somepca, geom.ind = "point", col.ind= matMeTu$subgr , palette = c("#00AFBB", "orange", "green", "purple"), axes.linetype = "blank", legend.title="Group", axes = c(1,4))
-# fviz_pca_ind(somepca, geom.ind = "point", col.ind= matMeTu$subgr , palette = c("#00AFBB", "orange", "green", "purple"), axes.linetype = "blank", legend.title="Group", axes = c(1,5))
-# fviz_eig(somepca, addlabels = TRUE)
-# fviz_pca_var(res.pca, col.var = "black")  # for correlation circle of the variables, important: variables need different names
+somepca <- PCA(MeTuconnect, scale.unit = TRUE, graph = FALSE)
+fviz_pca_ind(somepca, geom.ind = "point", col.ind= matMeTu$subgr , palette = c("#00AFBB", "#E7B800", "orange", "green", "purple"), axes.linetype = "blank", legend.title="Group")
+fviz_eig(somepca, addlabels = TRUE)
+
+
+somepca <- PCA(MeTuconnect, scale.unit = FALSE, graph = FALSE)
+fviz_pca_ind(somepca, geom.ind = "point", col.ind= matMeTu$subgr , palette = c("#00AFBB", "orange", "green", "purple"), axes.linetype = "blank", legend.title="Group")
+fviz_pca_ind(somepca, geom.ind = "point", col.ind= matMeTu$subgr , palette = c("#00AFBB", "orange", "green", "purple"), axes.linetype = "blank", legend.title="Group",axes = c(1,3))
+fviz_pca_ind(somepca, geom.ind = "point", col.ind= matMeTu$subgr , palette = c("#00AFBB", "orange", "green", "purple"), axes.linetype = "blank", legend.title="Group", axes = c(2,3))
+fviz_pca_ind(somepca, geom.ind = "point", col.ind= matMeTu$subgr , palette = c("#00AFBB", "orange", "green", "purple"), axes.linetype = "blank", legend.title="Group", axes = c(1,4))
+fviz_pca_ind(somepca, geom.ind = "point", col.ind= matMeTu$subgr , palette = c("#00AFBB", "orange", "green", "purple"), axes.linetype = "blank", legend.title="Group", axes = c(1,5))
+fviz_eig(somepca, addlabels = TRUE)
+fviz_pca_var(res.pca, col.var = "black")  # for correlation circle of the variables, important: variables need different names
 # more: contribution of variables to PCi, variables according to their contributions to the principal components (dimension description)
 
 
 ## Iterating over the 4 subcategories, we will extract the data for each category and perform PCA on one category per time
-## Firstly, let us set a variable for the column containing the grouping so as to exclude this column from the analysis
+## Firstly, let us set a variabl
+print("analyze aggregated group sube for the column containing the grouping so as to exclude this column from the analysis
 colgroup = 75
-## Category sub1:
-print("analyze aggregated group sub1")
+## Category sub1:1")
 nonzcol2 <- PCAanalysis_metu(matMeTu[which(matMeTu$subgr == 2),-colgroup], "/home/alikiz/Documents/WernerLab/plots/groupsub1.pdf")
 table(nonzcol2)
 
@@ -140,7 +142,10 @@ print("analyze group sub4")
 nonzcol5 <- PCAanalysis_metu(matMeTu[which(matMeTu$subgr == 5),-colgroup], "/home/alikiz/Documents/WernerLab/plots/groupsub4.pdf")
 table(nonzcol5)
 
-
+## Category sub4:
+print("analyze group 3 and 4")
+nzcol5 <- PCAanalysis_metu(matMeTu[which(matMeTu$subgr == 5 & matMeTu$subgr == 4),-colgroup], "/home/alikiz/Documents/WernerLab/plots/groupsub4and3.pdf")
+table(nonzcol5)
 
 # # new matrix: comb, combination of both 
 # comb = cbind(connect_mat, mat_both)
@@ -148,3 +153,9 @@ table(nonzcol5)
 # comb_pca = PCA(new, scale.unit = TRUE, graph = FALSE)
 # fviz_pca_ind(comb_pca, geom.ind = "point", col.ind= new_groups$label, palette = c("#00AFBB", "#E7B800"), axes.linetype = "blank", legend.title="Group")
 # fviz_eig(comb_pca, addlabels = TRUE)
+
+# TuBu.info = neuprint_search(".*TuBu.*")
+# m =neuprint_get_adjacency_matrix(inputids = MeTu.info$bodyid, outputids = TuBu.info$bodyid)
+# colnames(m) = TuBu.info$name
+# rownames(m) = MeTu.info$bodyid
+# meeetuu = m[rownames(m) %in% group$bodyid,]
